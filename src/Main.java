7883f695a620cd1b9b146c51e3a8b1906d697210
Irina <kalashiri-sweet@mail.ru>
2023-02-07 19:35:06 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {System.out.println("Спасибо, что выбрали нашу Авиакомпанию.Ваши бонусные мили составили");
+        int amount = 15000;// цена авиабилета руб
+        int x = 20;// постоянная переменная руб
+        int bonus = amount/x;
+        System.out.println((bonus) + " миль ");
     }
 }
