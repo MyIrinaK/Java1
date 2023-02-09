893d9fe5a0e1c935f8935e4c26cdcb33af39e25a
Irina <kalashiri-sweet@mail.ru>
2023-02-09 11:12:30 +0500

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
-    public static void main(String[] args) {System.out.println("Спасибо, что выбрали нашу Авиакомпанию.Ваши бонусные мили составили");
+    public static void main(String[] args) {
+        System.out.println("Спасибо, что выбрали нашу Авиакомпанию.Ваши бонусные мили составили");
         int amount = 15000;// цена авиабилета руб
-        int x = 20;// постоянная переменная руб
-        int bonus = amount/x;
+        int oneMiles = 20;// стоимость одной бонусной мили  руб
+        int bonus = amount / oneMiles;
         System.out.println((bonus) + " миль ");
     }
 }
